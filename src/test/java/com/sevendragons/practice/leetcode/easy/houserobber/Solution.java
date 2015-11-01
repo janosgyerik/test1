2abd1f48b46efab939d671778e0c536252fd65fe
Janos Gyerik <info@janosgyerik.com>
2015-11-01 15:07:01 +0000

    should be compiling but not...

--- src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/Solution.java
+++ src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/Solution.java
@@ -1,45 +1,22 @@
-package com.sevendragons.practice.leetcode.medium.houserobber2;
+package com.sevendragons.practice.leetcode.easy.houserobber;
 
 public class Solution {
     public int rob(int[] nums) {
-        int length = nums.length;
-
-        if (length == 0) {
-            return 0;
-        }
-
-        if (length == 1) {
-            return nums[0];
-        }
-
-        // choose first num, so don't count last
-        int backup = nums[length - 1];
-        nums[length - 1] = 0;
-        int alt1 = robHelper(nums);
-        nums[length - 1] = backup;
-
-        // don't count first num
-        nums[0] = 0;
-        int alt2 = robHelper(nums);
-
-        // don't count first two nums
-        nums[1] = 0;
-        int alt3 = robHelper(nums);
-
-        return Math.max(alt1, Math.max(alt2, alt3));
+        
     }
 
-    private int robHelper(int[] nums) {
-        int length = nums.length;
-        int maxAtPrevPrev = nums[0];
-        int maxAtPrev = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < length; ++i) {
-            int current = nums[i];
-            int max = Math.max(maxAtPrevPrev + current, maxAtPrev);
-            maxAtPrevPrev = maxAtPrev;
-            maxAtPrev = max;
+    //remove index and adjacent ind
+    // 1, [3, 1, 4] -> []
+    // 3, [1, 4, 3, 6, 7] -> [1,4]
+    //              ^
+    // 3, [1, 4, 3, 6, 7, 2] -> [1,4,2]
+    //              ^
+    public int[] removeIndexAndNeighbors(int[] nums, int index) {
+        if (nums.length <= 3) {
+            return new int[0];
         }
-        return maxAtPrev;
+        int[] toReturn = new int[nums.length - 3];
+        System.arraycopy(nums, 0, toReturn, 0, );
+        
     }
 }

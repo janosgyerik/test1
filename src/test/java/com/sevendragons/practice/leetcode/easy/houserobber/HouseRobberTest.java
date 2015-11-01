463f5b0b1ae289f75e4d03434b2f652836516d50
Janos Gyerik <info@janosgyerik.com>
2015-11-01 16:34:56 +0000

    solution accepted

--- src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/HouseRobberTest.java
+++ src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/HouseRobberTest.java
@@ -69,7 +69,7 @@ public class HouseRobberTest {
     }
     
     @Test
-    public void test_time_limit_exceeded_example() {
+    public void test_rob_time_limit_exceeded_example() {
         assertEquals(0, solution.rob(new int[]{183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211}));
     }
 }
--- src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/Solution.java
+++ src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/Solution.java
@@ -1,14 +1,27 @@
 package com.sevendragons.practice.leetcode.easy.houserobber;
 
+import java.util.*;
+
 public class Solution {
     
     public int rob(int[] nums) {
+        return rob(nums, new HashMap<Integer, Integer>());
+    }
+
+    // map: # of remaining houses -> max sum
+    public int rob(int[] nums, Map<Integer, Integer> results) {
         if (nums.length == 0) {
             return 0;
         }
-        int a = nums[0] + rob(tail(nums, 2));  // robbing nums[0]
-        int b = rob(tail(nums, 1));            // not robbing nums[0]
-        return Math.max(a, b);
+        if (results.containsKey(nums.length)) {
+            return results.get(nums.length);
+        }
+        int a = nums[0] + rob(tail(nums, 2), results);  // robbing nums[0]
+        int b = rob(tail(nums, 1), results);            // not robbing nums[0]
+        
+        int result = Math.max(a, b);
+        results.put(nums.length, result);
+        return result;
     }
     
     public int[] tail(int[] nums, int skip) {
@@ -19,34 +32,4 @@ public class Solution {
         System.arraycopy(nums, skip, tail, 0, tail.length);
         return tail;
     }
-
-    //remove index and adjacent ind
-    // 1, [3, 1, 4] -> []
-    // 3, [1, 4, 3, 6, 7] -> [1,4]
-    //              ^
-    // 3, [1, 4, 3, 6, 7, 2] -> [1,4,2]
-    //              ^
-    public int[] removeIndexAndNeighbors(int[] nums, int index) {
-        if (nums.length <= 3) {
-            return new int[0];
-        }
-        
-        int[] toReturn;
-        if (index == 0) {
-            toReturn = new int[nums.length - 2];
-            // [1, 4, 2, 3], 0 -> [2, 3]
-            System.arraycopy(nums, 2, toReturn, 0, toReturn.length);
-        } else if (index == nums.length - 1) {
-            // [1, 4, 2, 3], 3 -> [1, 4]
-            toReturn = new int[nums.length - 2];
-            System.arraycopy(nums, 0, toReturn, 0, toReturn.length);
-        } else {
-            toReturn = new int[nums.length - 3];
-            System.arraycopy(nums, 0, toReturn, 0, index - 1);
-            System.arraycopy(nums, index + 2, toReturn, index - 1, nums.length - index - 2);
-        }
-        return toReturn;
-    }
-    
-    
 }

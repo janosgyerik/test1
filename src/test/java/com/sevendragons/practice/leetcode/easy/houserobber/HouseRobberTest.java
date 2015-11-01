9a3c12d02e9c457f8edbd3350c4f3cb069712957
Janos Gyerik <info@janosgyerik.com>
2015-11-01 16:42:58 +0000

    removed unused tests, and fixed tle example test

--- src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/HouseRobberTest.java
+++ src/test/java/com/sevendragons/practice/leetcode/easy/houserobber/HouseRobberTest.java
@@ -9,31 +9,6 @@ public class HouseRobberTest {
     private final Solution solution = new Solution();
     
     @Test
-    public void test_removeIndexAndNeighbors_size3() {
-        assertArrayEquals(new int[0], solution.removeIndexAndNeighbors(new int[]{3, 1, 4}, 2));
-    }
-
-    @Test
-    public void test_removeIndexAndNeighbors_size4() {
-        assertArrayEquals(new int[]{2}, solution.removeIndexAndNeighbors(new int[]{3, 1, 4, 2}, 1));
-    }
-
-    @Test
-    public void test_removeIndexAndNeighbors_1_4_3_6_7_2_x_3_gives_1_4_2() {
-        assertArrayEquals(new int[]{1, 4, 2}, solution.removeIndexAndNeighbors(new int[]{1, 4, 3, 6, 7, 2}, 3));
-    }
-
-    @Test
-    public void test_removeIndexAndNeighbors_1_4_3_6_x_0_gives_3_6() {
-        assertArrayEquals(new int[]{3, 6}, solution.removeIndexAndNeighbors(new int[]{1, 4, 3, 6}, 0));
-    }
-
-    @Test
-    public void test_removeIndexAndNeighbors_1_4_3_6_x_3_gives_1_4() {
-        assertArrayEquals(new int[]{1, 4}, solution.removeIndexAndNeighbors(new int[]{1, 4, 3, 6}, 3));
-    }
-    
-    @Test
     public void test_tail_9_x_1() {
         assertArrayEquals(new int[0], solution.tail(new int[]{9}, 1));
     }
@@ -70,6 +45,6 @@ public class HouseRobberTest {
     
     @Test
     public void test_rob_time_limit_exceeded_example() {
-        assertEquals(0, solution.rob(new int[]{183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211}));
+        assertEquals(3365, solution.rob(new int[]{183,219,57,193,94,233,202,154,65,240,97,234,100,249,186,66,90,238,168,128,177,235,50,81,185,165,217,207,88,80,112,78,135,62,228,247,211}));
     }
 }

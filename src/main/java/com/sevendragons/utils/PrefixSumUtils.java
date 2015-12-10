0843747d12fdee3bbf708f9515a939e3351a4a97
Janos Gyerik <janos@kronos>
2015-12-10 21:43:28 +0100

    added PrefixSumUtils

---
+++ src/main/java/com/sevendragons/utils/PrefixSumUtils.java
@@ -0,0 +1,25 @@
+package com.sevendragons.utils;
+
+/**
+ * Prefix sums provide a simple yet powerful technique that allows for the
+ * fast calculation of sums of elements in given contiguous segments of arrays.
+ * Prefix sums are defined as the consecutive totals of the first 0, 1, 2, . . . , n elements of an array.
+ *
+ * See also:
+ * https://codility.com/programmers/lessons/3/
+ * https://codility.com/media/train/3-PrefixSums.pdf
+ */
+public final class PrefixSumUtils {
+
+    private PrefixSumUtils() {
+        throw new AssertionError("utility class, forbidden constructor");
+    }
+
+    public static int[] prefixSums(int[] arr) {
+        int[] sums = new int[arr.length + 1];
+        for (int i = 1; i < arr.length + 1; ++i) {
+            sums[i] = sums[i - 1] + arr[i - 1];
+        }
+        return sums;
+    }
+}

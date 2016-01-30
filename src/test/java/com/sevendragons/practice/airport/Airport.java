252467a54a0b5dfae996e08aac7444859757f52d
Janos Gyerik <janos@kronos>
2016-01-30 16:16:18 +0100

    simplified readMatrix without error handling

--- src/test/java/com/sevendragons/practice/airport/Airport.java
+++ src/test/java/com/sevendragons/practice/airport/Airport.java
@@ -199,15 +199,10 @@ public class Airport {
 
     public static int[][] readMatrix(Scanner scanner, int dimensions) {
         int[][] matrix = new int[dimensions][dimensions];
-        for (int i = 0; i < dimensions; i++){
+        for (int i = 0; i < dimensions; i++) {
             String[] values = scanner.nextLine().split(",");
-            for (int j = 0; j < values.length; j++){
-                try{
-                    matrix[i][j] = Integer.valueOf(values[j]);
-                } catch (NumberFormatException e) {
-                    System.err.println("Error when parsing line "+i+" column "+j+": not parsable value "+values[j]);
-                }
-
+            for (int j = 0; j < values.length; j++) {
+                matrix[i][j] = Integer.valueOf(values[j]);
             }
         }
         return matrix;

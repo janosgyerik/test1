13e773b22e7772272176daa92bdd36eb9c4558c7
pierre <you@example.com>
2016-01-30 11:50:36 +0100

    add symetric method

--- src/test/java/com/sevendragons/practice/airport/AirportTest_pierre.java
+++ src/test/java/com/sevendragons/practice/airport/AirportTest_pierre.java
@@ -1,4 +1,29 @@
 package com.sevendragons.practice.airport;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AirportTest_pierre {
+    private Airport_pierre airport =  new Airport_pierre();
+
+
+    @Test
+    public void testSymetric_example() throws Exception {
+        int[][] transitMatrix = {{0,100,6},{60,0,8},{4,2,0}};
+        int[][] transitMatrixExpected = {{0,160,10},{160,0,10},{10,10,0}};
+
+        Assert.assertArrayEquals(transitMatrixExpected,airport.toSymetric(transitMatrix));
+
+
+    }
+
+    @Test
+    public void testSymetric_example_alreadySymetric() throws Exception {
+        int[][] transitMatrix = {{1,0,0},{0,1,0},{0,0,1}};
+
+        Assert.assertArrayEquals(transitMatrix,airport.toSymetric(transitMatrix));
+
+
+    }
+
 }
--- src/test/java/com/sevendragons/practice/airport/Airport_pierre.java
+++ src/test/java/com/sevendragons/practice/airport/Airport_pierre.java
@@ -1,4 +1,26 @@
 package com.sevendragons.practice.airport;
 
 public class Airport_pierre {
+
+
+    public int[][] toSymetric(int[][] matrix){
+        int lengthmatrix = matrix.length;
+        int [][] matrix_symetric = new int[lengthmatrix][lengthmatrix];
+
+        for(int i = 0; i<lengthmatrix;i++){
+            for(int j= 0 ; j< lengthmatrix;j++){
+                if(j!=i){
+                    matrix_symetric[i][j]= matrix[i][j]+matrix[j][i];
+
+                }else{
+                    matrix_symetric[i][j]= matrix[i][j];
+                }
+
+            }
+
+        }
+
+        return matrix_symetric;
+
+    }
 }

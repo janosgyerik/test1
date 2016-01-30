c9d1f52635defeef2a0cf0a0ce902328d0e11299
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2016-01-30 11:12:21 +0000

    Fix Reader and add unit test

--- src/test/java/com/sevendragons/practice/airport/AirportTest_pj.java
+++ src/test/java/com/sevendragons/practice/airport/AirportTest_pj.java
@@ -1,4 +1,24 @@
 package com.sevendragons.practice.airport;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.Scanner;
+
 public class AirportTest_pj {
+
+    @Test
+    public void readDimensions(){
+        Scanner scanner = new Scanner("3\n" +
+                "0,10,12\n" +
+                "10,0,5\n" +
+                "12,5,0\n" +
+                "0,100,6\n" +
+                "60,0,8\n" +
+                "4,2,0");
+        assertEquals(Airport_pj.readDimensions(scanner), 3);
+        assertArrayEquals(Airport_pj.readMatrix(scanner, 3),new int[][]{{0,10,12},{10,0,5},{12,5,0}});
+        assertArrayEquals(Airport_pj.readMatrix(scanner, 3),new int[][]{{0,100,6},{60,0,8},{4,2,0}});
+    }
+
 }
--- src/test/java/com/sevendragons/practice/airport/Airport_pj.java
+++ src/test/java/com/sevendragons/practice/airport/Airport_pj.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Airport_pj {
 
     public static int readDimensions(Scanner scanner){
-        return scanner.nextInt();
+        return Integer.valueOf(scanner.nextLine());
     }
 
     public static int[][] readMatrix(Scanner scanner, int dimensions){
@@ -13,7 +13,12 @@ public class Airport_pj {
         for (int i = 0; i < dimensions; i++){
             String[] values = scanner.nextLine().split(",");
             for (int j = 0; j < values.length; j++){
-                matrix[i][j] = Integer.valueOf(values[j]);
+                try{
+                    matrix[i][j] = Integer.valueOf(values[j]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Error when parsing line "+i+" column "+j+": not parsable value "+values[j]);
+                }
+
             }
         }
         return matrix;

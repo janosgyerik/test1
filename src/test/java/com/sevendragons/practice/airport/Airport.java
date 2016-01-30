db11097158d2ab0b69930e53a238c5de36b76f87
Janos Gyerik <janos@kronos>
2016-01-30 11:23:28 +0100

    added edge classes + sorting

--- src/test/java/com/sevendragons/practice/airport/Airport.java
+++ src/test/java/com/sevendragons/practice/airport/Airport.java
@@ -1,4 +1,63 @@
 package com.sevendragons.practice.airport;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Airport {
+    public static class Edge {
+        protected final int start;
+        protected final int end;
+        protected final int value;
+
+        public Edge(int start, int end, int value) {
+            this.start = start;
+            this.end = end;
+            this.value = value;
+        }
+    }
+
+    public static class TransitEdge extends Edge {
+        public TransitEdge(int start, int end, int value) {
+            super(start, end, value);
+        }
+
+        public int startFlight() {
+            return start;
+        }
+
+        public int endFlight() {
+            return end;
+        }
+
+        public int passengers() {
+            return value;
+        }
+    }
+
+    public static class DistanceEdge extends Edge {
+        public DistanceEdge(int start, int end, int value) {
+            super(start, end, value);
+        }
+
+        public int startGate() {
+            return start;
+        }
+
+        public int endGate() {
+            return end;
+        }
+
+        public int distance() {
+            return value;
+        }
+    }
+
+    public static void sortByPassengersDescending(List<TransitEdge> transitEdges) {
+        Collections.sort(transitEdges, (o1, o2) -> -Integer.compare(o1.passengers(), o2.passengers()));
+    }
+
+    public static void sortByDistanceAscending(List<DistanceEdge> distanceEdges) {
+        Collections.sort(distanceEdges, (o1, o2) -> Integer.compare(o1.distance(), o2.distance()));
+    }
+
 }
--- src/test/java/com/sevendragons/practice/airport/AirportTest.java
+++ src/test/java/com/sevendragons/practice/airport/AirportTest.java
@@ -1,4 +1,36 @@
 package com.sevendragons.practice.airport;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.sevendragons.practice.airport.Airport.*;
+import static org.junit.Assert.assertEquals;
+
 public class AirportTest {
+    @Test
+    public void test_sorted_transit_by_passengers_descending() {
+        TransitEdge transitEdge1 = new TransitEdge(1, 2, 1);
+        TransitEdge transitEdge2 = new TransitEdge(3, 4, 2);
+        TransitEdge transitEdge5 = new TransitEdge(5, 6, 5);
+
+        List<TransitEdge> transitEdges = Arrays.asList(transitEdge2, transitEdge1, transitEdge5);
+        sortByPassengersDescending(transitEdges);
+
+        assertEquals(Arrays.asList(transitEdge5, transitEdge2, transitEdge1), transitEdges);
+    }
+
+    @Test
+    public void test_sorted_by_distance_ascending() {
+        DistanceEdge distanceEdge1 = new DistanceEdge(1, 2, 1);
+        DistanceEdge distanceEdge2 = new DistanceEdge(3, 4, 2);
+        DistanceEdge distanceEdge5 = new DistanceEdge(5, 6, 5);
+
+        List<Airport.DistanceEdge> distanceEdges = Arrays.asList(distanceEdge2, distanceEdge1, distanceEdge5);
+        sortByDistanceAscending(distanceEdges);
+
+        assertEquals(Arrays.asList(distanceEdge1, distanceEdge2, distanceEdge5), distanceEdges);
+    }
+
 }

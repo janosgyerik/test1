830c1659744b63707cbcc8b9c7cda6410519bd8d
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2016-01-30 10:45:47 +0000

    Addding Entry Parsing method

--- src/test/java/com/sevendragons/practice/airport/Airport_pj.java
+++ src/test/java/com/sevendragons/practice/airport/Airport_pj.java
@@ -1,4 +1,21 @@
 package com.sevendragons.practice.airport;
 
+import java.util.Scanner;
+
 public class Airport_pj {
+
+    public static int readDimensions(Scanner scanner){
+        return scanner.nextInt();
+    }
+
+    public static int[][] readMatrix(Scanner scanner, int dimensions){
+        int[][] matrix = new int[dimensions][dimensions];
+        for (int i = 0; i < dimensions; i++){
+            String[] values = scanner.nextLine().split(",");
+            for (int j = 0; j < values.length; j++){
+                matrix[i][j] = Integer.valueOf(values[j]);
+            }
+        }
+        return matrix;
+    }
 }
